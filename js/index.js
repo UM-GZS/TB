c64fe470ec8e67575305742487b4f794fcb9d4dc
UMGZS <umgzs01@gamil.com>
2020-07-22 17:41:21 +0800

    mox

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
-/* $(document).ready(function() {
-	$(".show_other").hover(function() {
-		$(".show_address").show();
-	});
-});
- */
+window.onload = function() {
+	var swiper = document.getElementById('swiper'),
+	pic = document.getElementById('swiper_ul').getElementsByTagName("li"),
+	list = document.getElementById('swiper_ol').getElementsByTagName("li"),
+	index = 0,
+	timer = null;
+	
+	timer = setInterval(autoPlay, 2000);
+	
+	swiper.onmouseover = function () {
+		clearInterval(timer);
+	}
+	
+	swiper.onmouseout = function () {
+		timer = setInterval(autoPlay, 2000);
+	}
+	
+	for(var i = 0;i < list.length;i++) {
+		list[i].onmouseover = function () {
+			clearInterval(timer);
+			index = list.getAttribute("title") - 1;
+			console.log("data is :"+index)
+			changePic(index);
+		}
+	}
+	
+	function autoPlay () {
+		if(++index >= pic.length) index = 0;
+		changePic(index);
+	}
+	
+	function changePic (curIndex) {
+	 for (var i = 0; i < pic.length; ++i) {
+	 pic[i].style.display = "none";
+	 list[i].className = "";
+	 }
+	 pic[curIndex].style.display = "block";
+	 list[curIndex].className = "swiper_on";
+	 }
+}
