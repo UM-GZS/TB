a12ab74bf60f4869a305e53dbada51c634646ebb
UMGZS <umgzs01@gmail.com>
2020-07-28 00:04:24 +0800

    kolboy

--- js/index.js
+++ js/index.js
@@ -2,17 +2,30 @@ window.onload = function() {
 	var swiper = document.getElementById('swiper'),
 	pic = document.getElementById('swiper_ul').getElementsByTagName("li"),
 	list = document.getElementById('swiper_ol').getElementsByTagName("li"),
+	swiper_two = document.getElementById('swiper_two'),
+	pic_two = document.getElementById('swiper_ul_two').getElementsByTagName("li"),
+	list_two = document.getElementById('swiper_ol_two').getElementsByTagName("li"),
+	swiper_span = document.getElementById('swiper_span'),
 	index = 0,
-	timer = null;
+	index_two = 0,
+	timer = null,
+	timer_two = null;
 	
 	timer = setInterval(autoPlay, 4000);
+	timer_two = setInterval(autoPlayTwo, 4000);
 	
 	swiper.onmouseover = function () {
 		clearInterval(timer);
 	}
+	swiper_two.onmouseover = function () {
+		clearInterval(timer_two);
+	}
 	
 	swiper.onmouseout = function () {
 		timer = setInterval(autoPlay, 4000);
+	}
+	swiper_two.onmouseout = function () {
+		timer = setInterval(changePicTwo, 4000);
 	}
 	
 	for(var i = 0;i < list.length;i++) {
@@ -22,10 +35,22 @@ window.onload = function() {
 			changePic(index);
 		}
 	}
+	for(var i = 0;i < list_two.length;i++) {
+		list_two[i].onmouseover = function () {
+			index_two = this.getAttribute("data") - 1;
+			console.log("data is :"+index_two);
+			clearInterval(timer_two);
+			changePicTwo(index_two);
+		}
+	}
 	
 	function autoPlay () {
 		if(++index >= pic.length) index = 0;
 		changePic(index);
+	}
+	function autoPlayTwo () {
+		if(++index_two >= pic_two.length) index_two = 0;
+		changePicTwo(index_two);
 	}
 	
 	function changePic (curIndex) {
@@ -36,4 +61,14 @@ window.onload = function() {
 	 pic[curIndex].style.display = "block";
 	 list[curIndex].className = "swiper_on";
 	 }
+	 function changePicTwo (curIndex) {
+		 
+	  for (var i = 0; i < pic_two.length; ++i) {
+	  pic_two[i].style.display = "none";
+	  list_two[i].className = "";
+	  }
+	  pic_two[curIndex].style.display = "block";
+	  list_two[curIndex].className = "s_on";
+	  swiper_span.innerHTML = curIndex+1;
+	  }
 }
